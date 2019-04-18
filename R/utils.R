f3801998f4257a45d063413a89038ea0f99e1b11
sgraham9319 <sg9319@my.bristol.ac.uk>
2019-04-18 13:41:11 -0700

    Created script for beta diversity decomposition calculations

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,13 @@
 
+
+#=========================
+# Calculate standard error
+#=========================
+
+se <- function(x){
+  sd(x, na.rm = T) / sqrt(length(na.omit(x)))
+}
+
 #===========================================================================
 # Add Faith's PD and species richness as columns to a site by species matrix
 #===========================================================================
@@ -47,4 +56,37 @@ match_phylo_names <- function(dat){
   supertree_names <- as.character(species_key$representative_in_supertree[relev_cols])
   colnames(dat)[7:62] <- supertree_names
   dat
+}
+
+#=====================================================
+# Summarize decomposed beta diversity by land use type
+#=====================================================
+
+beta_decomp <- function(com_mat, phylo, sps_cols){
+  
+  # Create dataframe to store decomposition results
+  results <- as.data.frame(matrix(NA, ncol = 5, nrow = length(unique(s_mamm$pair_id))))
+  colnames(results) <- c("pair_id", "landuse", "beta_PD", "turnover", "nestedness")
+  results$pair_id <- unique(s_mamm$pair_id)
+  results$landuse <- s_mamm[s_mamm$landuse != "Conserved", ]$landuse
+  
+  # Loop through pairs, decomposing beta pd for each
+  for(pair in 1:nrow(results)){
+    com <- com_mat[com_mat$pair_id == pair, sps_cols]
+    results[results$pair_id == pair, 3:5] <- beta.pd.decompo(com = com, tree = phylo, 
+                                                             type = "Unifrac")$betadiv
+  }
+  
+  # Summarize output
+  output <- results %>% group_by(landuse) %>% 
+    summarise(total_av = mean(beta_PD, na.rm = T),
+              total_sd = sd(beta_PD, na.rm = T),
+              turnover_av = mean(turnover, na.rm = T),
+              turnover_sd = sd(turnover, na.rm = T),
+              turnover_se = se(turnover),
+              nested_av = mean(nestedness, na.rm = T),
+              nested_sd = sd(nestedness, na.rm = T))
+  
+  # Return output
+  output
 }
